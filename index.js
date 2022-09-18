22ba3eed80ce15337b625565c231aad99b2294ed
James <jamessoh11@gmail.com>
2022-09-18 12:57:03 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,83 @@
+const { response } = require("express");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(express.json());
+app.use(cors());
+
+app.use(requestLogger);
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello world</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => {
+    return note.id === id;
+  });
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.post("/api/notes", (request, response) => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  const note = request.body;
+  console.log(note);
+  response.json(note);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((item) => item.id !== id);
+  response.status(204).end();
+});
+
+// Catch non routes
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
